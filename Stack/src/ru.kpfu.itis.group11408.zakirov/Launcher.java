7ecd2152764611834b2ae3ffa9c7b055235506e3
anvarzkr <anvarzkr@gmail.com>
2015-03-23 21:56:42 +0300

    + Stack HW

---
+++ Stack/src/ru.kpfu.itis.group11408.zakirov/Launcher.java
@@ -0,0 +1,31 @@
+package ru.kpfu.itis.group11408.zakirov;
+
+import java.util.Scanner;
+
+/**
+ * Created by Anvar on 23.03.2015.
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        SuperStack<Integer> superStack = new SuperStack<>();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please, write brackets sequence.");
+        System.out.println("Pojalyusta, vvedite posledovatelnost skobok.");
+        String str = scanner.next();
+
+        for (int i = 0; i < str.length(); i++){
+            if (str.charAt(i) == '('){
+                superStack.push(1);
+            }else{
+                if (superStack.size() == 0){
+                    System.out.println("Sequence of brackets is WRONG!");
+                    return;
+                }else{
+                    superStack.pop();
+                }
+            }
+        }
+
+        System.out.println("Sequence of brackets is " + ((superStack.size() == 0) ? "RIGHT" : "WRONG") + "!");
+    }
+}
---
+++ Stack/src/ru.kpfu.itis.group11408.zakirov/SuperStack.java
@@ -0,0 +1,47 @@
+package ru.kpfu.itis.group11408.zakirov;
+
+import java.util.*;
+
+/**
+ * Created by Anvar on 23.03.2015.
+ */
+public class SuperStack<T>{
+    private final int MAX_SIZE = 100;
+    private T[] stack = (T[])new Object[MAX_SIZE];
+    private int size = 0;
+
+    public T pop(){
+        if (size == 0)
+            throw new EmptyStackException();
+        T returned = this.stack[--size];
+        this.stack[size] = null;
+        return returned;
+    }
+
+    public T peek(){
+        if (size == 0)
+            throw new EmptyStackException();
+        return this.stack[size - 1];
+    }
+
+    public void push(T obj){
+        this.stack[size++] = obj;
+    }
+
+    public int size(){
+        return this.size;
+    }
+
+    public boolean empty(){
+        return (this.size == 0);
+    }
+
+    public int search(Object obj){
+        for (int i = 0; i < this.size; i++)
+            if (obj.hashCode() == this.stack[i].hashCode())
+                return i;
+
+        return -1;
+    }
+
+}
