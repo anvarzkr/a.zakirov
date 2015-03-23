b37a0b0669ebbd1953067a8ad6d7e937c610e203
anvarzkr <anvarzkr@gmail.com>
2015-03-23 03:43:36 +0300

    + polyangle executor

---
+++ polyangle/exec_polyangle_Windows.bat
@@ -0,0 +1,3 @@
+javac -d bin -sourcepath src/ src/ru/kpfu/itis/group11408/zakirov/polyangle/Polyangle.java
+java -cp "bin" ru.kpfu.itis.group11408.zakirov.polyangle.Polyangle
+pause
--- polyangle/src/ru/kpfu/itis/group11408/zakirov/polyangle/Polyangle.java
+++ polyangle/src/ru/kpfu/itis/group11408/zakirov/polyangle/Polyangle.java
@@ -7,6 +7,15 @@ public class Polyangle {
     private Vertex[] vertexes;
 
     public static void main(String[] args) {
+        System.out.println("Example:");
+        System.out.println();
+        System.out.println("Polyangle with Vertexes(x, y):");
+        System.out.println("Vertex1: (0, 0)");
+        System.out.println("Vertex2: (2, 1)");
+        System.out.println("Vertex3: (0, 2)");
+        System.out.println("Vertex4: (3, 1)");
+        System.out.println();
+
         Polyangle polyangle = new Polyangle(new Vertex[]{
             new Vertex(0, 0),
             new Vertex(2, 1),
@@ -14,7 +23,22 @@ public class Polyangle {
             new Vertex(3, 1)
         });
 
-        System.out.println(polyangle.isSharp());
+        System.out.println("Answer: this polyangle is " + ((polyangle.isSharp()) ? "" : "NOT" ) + " SHARP(Vipukliy)!");
+
+        System.out.println();
+        System.out.println("Polyangle with Vertexes(x, y):");
+        System.out.println("Vertex1: (0, 0)");
+        System.out.println("Vertex3: (-1, 1)");
+        System.out.println("Vertex2: (1, 1)");
+        System.out.println();
+
+        polyangle = new Polyangle(new Vertex[]{
+                new Vertex(0, 0),
+                new Vertex(-1, 1),
+                new Vertex(1, 1)
+        });
+
+        System.out.println("Answer: this polyangle is " + ((polyangle.isSharp()) ? "" : "NOT" ) + " SHARP(Vipukliy)!");
     }
 
     public boolean isSharp(){
@@ -24,7 +48,7 @@ public class Polyangle {
         for (int i = 0; i < this.vertexes.length; i++){
             v0 = this.vertexes[i];
             v1 = (i != 0) ? this.vertexes[i - 1] : this.vertexes[this.vertexes.length - 1];
-            v2 = (i != this.vertexes.length) ? this.vertexes[i + 1] : this.vertexes[0];
+            v2 = (i != this.vertexes.length - 1) ? this.vertexes[i + 1] : this.vertexes[0];
 
             if ( (v1.x - v0.x) * (v2.y - v0.y) - (v2.x - v0.x) * (v1.y - v0.y) < 0)
                 isSharp = false;
